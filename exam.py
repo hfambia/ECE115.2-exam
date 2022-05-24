2475b38da2409354d11459c3e25f1a938c70f9ef
naizarpiang <105398867+naizarpiang@users.noreply.github.com>
2022-05-24 10:50:55 +0800

    Update exam.py

--- exam.py
+++ exam.py
@@ -7,15 +7,15 @@ class StopWatch(Frame):
             
 def main():
     root = Tk()
+    root.title('Exam')
+    root.geometry("250x200")
+    root.resizable(False, False)
     root.wm_attributes("-topmost", 1)
     sw = StopWatch(root)
     sw.pack(side=TOP)
-    Button(root, text='Lap').pack(side=LEFT)
-    Button(root, text='Start').pack(side=LEFT)
-    Button(root, text='Stop').pack(side=LEFT)
-    Button(root, text='Reset').pack(side=LEFT)
-    Button(root, text='Quit').pack(side=LEFT)    
+    Button(root, text='Lap').pack(side=LEFT,fill=X, expand=YES, anchor=S)
+    Button(root, text='Start').pack(side=LEFT,fill=X, expand=YES, anchor=S)
+    Button(root, text='Stop').pack(side=LEFT,fill=X, expand=YES, anchor=S)
+    Button(root, text='Reset').pack(side=LEFT,fill=X, expand=YES, anchor=S)
+    Button(root, text='Quit').pack(side=LEFT,fill=X, expand=YES, anchor=S)
     root.mainloop()
-
-if __name__ == '__main__':
-    main()
