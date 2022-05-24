a46c7edc68947bfae357541f6e3de603e3c81a0c
hfambia <105398508+hfambia@users.noreply.github.com>
2022-05-24 11:48:18 +0800

    Update exam.py
    Fixed Variables and made start button have the command "Start"

--- exam.py
+++ exam.py
@@ -4,7 +4,9 @@ import time
 class StopWatch(Frame):                                                           
     def __init__(self, parent=None, **kw):        
         Frame.__init__(self, parent, kw)
+        self._start = 0.0        
         self._elapsedtime = 0.0
+        self._running = 0
         self.timestr = StringVar()
         self.makeWidgets()
         
@@ -12,9 +14,9 @@ class StopWatch(Frame):
         self.e = Entry(self)
         l = Label(self, textvariable=self.timestr)
         self._setTime(self._elapsedtime)
-        l.pack(fill=X, expand=NO, pady=4, padx=2)
+        l.pack(fill=X, expand=NO, pady=3, padx=2)
         l2 = Label(self, text='Laps')
-        l2.pack(fill=X, expand=NO, pady=4, padx=90)
+        l2.pack(fill=X, expand=NO, pady=4, padx=2)
         scrollbar = Scrollbar(self, orient=VERTICAL)
         self.m = Listbox(self,selectmode=EXTENDED, height = 5, yscrollcommand=scrollbar.set)
         self.m.pack(side=LEFT, fill=BOTH, expand=1, pady=5, padx=2)
@@ -25,17 +27,18 @@ class StopWatch(Frame):
         self._elapsedtime = time.time() - self._start
         self._setTime(self._elapsedtime)
         self._timer = self.after(50, self._update)
-       
+    
     def _setTime(self, elap):
         minutes = int(elap/60)
         seconds = int(elap - minutes*60.0)
         hseconds = int((elap - minutes*60.0 - seconds)*100)                
         self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
-        
+
     def Start(self):                                          
         if not self._running:            
             self._start = time.time() - self._elapsedtime
-            self._running = 1
+            self._update()
+            self._running = 1 
         
         
 def main():
@@ -47,7 +50,7 @@ def main():
     sw = StopWatch(root)
     sw.pack(side=TOP)
     Button(root, text='Lap').pack(side=LEFT,fill=X, expand=YES, anchor=S)
-    Button(root, text='Start').pack(side=LEFT,fill=X, expand=YES, anchor=S)
+    Button(root, text='Start',command=sw.Start).pack(side=LEFT,fill=X, expand=YES, anchor=S)
     Button(root, text='Stop').pack(side=LEFT,fill=X, expand=YES, anchor=S)
     Button(root, text='Reset').pack(side=LEFT,fill=X, expand=YES, anchor=S)
     Button(root, text='Quit').pack(side=LEFT,fill=X, expand=YES, anchor=S)
@@ -56,4 +59,3 @@ def main():
     
 if __name__ == '__main__':
     main()
-    
