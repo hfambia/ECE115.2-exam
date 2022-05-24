074aa176df5d160b4d93fd473a6423cc2cd8ad0d
hfambia <105398508+hfambia@users.noreply.github.com>
2022-05-24 11:30:23 +0800

    Update exam.py

--- exam.py
+++ exam.py
@@ -4,7 +4,30 @@ import time
 class StopWatch(Frame):                                                           
     def __init__(self, parent=None, **kw):        
         Frame.__init__(self, parent, kw)
-            
+        self._elapsedtime = 0.0
+        self.timestr = StringVar()
+        self.makeWidgets()
+        
+    def makeWidgets(self):                         
+        self.e = Entry(self)
+        l = Label(self, textvariable=self.timestr)
+        self._setTime(self._elapsedtime)
+        l.pack(fill=X, expand=NO, pady=4, padx=2)
+        l2 = Label(self, text='Laps')
+        l2.pack(fill=X, expand=NO, pady=4, padx=90)
+        scrollbar = Scrollbar(self, orient=VERTICAL)
+        self.m = Listbox(self,selectmode=EXTENDED, height = 5, yscrollcommand=scrollbar.set)
+        self.m.pack(side=LEFT, fill=BOTH, expand=1, pady=5, padx=2)
+        scrollbar.config(command=self.m.yview)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        
+    def _setTime(self, elap):
+        minutes = int(elap/60)
+        seconds = int(elap - minutes*60.0)
+        hseconds = int((elap - minutes*60.0 - seconds)*100)                
+        self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
+        
+        
 def main():
     root = Tk()
     root.title('Exam')
